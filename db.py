7d59b468d8e991e7b221b918bd857d4c46f0d7b8
lakamb <lakamb@bk.ru>
2023-07-19 23:30:38 +0300

    Update db.py

--- db.py
+++ db.py
@@ -6,16 +6,16 @@ async def db_start():
     db = sq.connect('mp.db')
     cur = db.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS Profile(user_id TEXT, datefrom TEXT, extra_field TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Profile(user_id TEXT, wb_token TEXT, from_dt TEXT, extra_field TEXT)")
 
     db.commit()
 
 async def create_profile(user_id):
     user = cur.execute("SELECT * FROM Profile WHERE user_id == '{key}'".format(key=user_id)).fetchone()
     if not user:
-        cur.execute("INSERT INTO Profile VALUES(?, ?, ?)", (user_id, '', ''))
+        cur.execute("INSERT INTO Profile VALUES(?, ?, ?, ?)", (user_id, '', '', ''))
 
 async def edit_profile(state, user_id):
     async with state.proxy() as data:
-        cur.execute("UPDATE Profile SET datefrom = '{}', extra_field = '{}' WHERE user_id == '{}' ".format(data['date_from'], '', user_id))
+        cur.execute("UPDATE Profile SET wb_token = '{}', from_dt = '{}', extra_field = '{}' WHERE user_id == '{}' ".format(data['wb_token'], data['from_dt'], '', user_id))
         db.commit()
